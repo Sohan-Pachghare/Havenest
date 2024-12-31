7b875cd18e12b901e20207d7eecb0547e05f0aa0
Sohan-Pachghare <sohanpachghare17@gmail.com>
2024-12-31 19:38:32 +0530

    added many things :)

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ const path = require("path");
 const mongoose = require('mongoose');
 const Listing = require("./models/listing");
 const methodOverride = require("method-override");
-const { log } = require("console");
-
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.urlencoded({extended : true}));
-app.use(methodOverride("_method"));
+const ejsMate = require('ejs-mate');
 
 main()
     .then(() => { console.log("connected to mongoDB"); })
@@ -20,6 +13,12 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 //index route
 app.get("/listings", async (req, res) => {
