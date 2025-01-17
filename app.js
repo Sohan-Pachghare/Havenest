6186e9e44c537c1bd83b448483977eca5cb9d1da
Sohan-Pachghare <sohanpachghare17@gmail.com>
2025-01-18 00:00:19 +0530

    Fix formatting in session cookie options

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const sessionOptions = {
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true
+        httpOnly: true,
     }
 }
 
